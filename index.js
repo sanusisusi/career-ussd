99d233eca9e35d76ebe770466d02ccd966b92e66
Sanusi Abdulkadir <94573138+sanusisusi@users.noreply.github.com>
2023-10-17 15:48:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,3 @@
-{/*require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const mongoString = process.env.DATABASE_URL;
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-database.on("error", (error) => {
-console.log(error);
-});
-database.once("connected", () => {
-console.log("Database connected...");
-});
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.listen(3000, () => {
-console.log("What's popping? We're connected");
-});*
-
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-
-const mongoString = process.env.DATABASE_URL;
-
-// Ensure that the MongoDB connection string starts with "mongodb://" or "mongodb+srv://"
-if (!mongoString.startsWith("mongodb://") && !mongoString.startsWith("mongodb+srv://")) {
-  console.error("Invalid MongoDB connection string. It should start with 'mongodb://' or 'mongodb+srv://'");
-  process.exit(1); // Exit the process if the connection string is invalid
-}
-
-mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.error("MongoDB connection error:", error);
-});
-
-database.once("connected", () => {
-  console.log("Database connected...");
-});
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(3000, () => {
-  console.log("What's popping? We're connected");
-});*/}
-
 // index.js
 require('dotenv').config();
 const express = require('express');
@@ -59,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/ussd', ussdRouter);
+app.use('/', ussdRouter);
+console.log(ussdRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
